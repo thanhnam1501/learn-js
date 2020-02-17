9c4ebf01683ffdbffe0adae275f1a79f6bcb927f
NamLemon <thanhnam1501@gmail.com>
2020-02-17 16:52:57 +0700

    bai 31

--- lesson31/index.js
+++ lesson31/index.js
@@ -1,5 +1,19 @@
 const readlineSync = require('readline-sync');
+const passwordGenerator = require('password-generator');
 
 let name = readlineSync.question('What is your name?: ');
-
 console.log(name);
+
+let password = passwordGenerator();
+console.log(password);
+
+const markdown = require( "markdown" ).markdown;
+console.log( markdown.toHTML( "Hello *World*!" ) );
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', client => {
+    client.on('event', data => { /* … */ });
+    client.on('disconnect', () => { /* … */ });
+});
+server.listen(3000);
